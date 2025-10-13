fcf84769e2074e43f7b1e3642561d02de76a970c
ahmadissa22145-cell <ahmadissa22145@gmail.com>
2025-10-13 10:54:16 +0300

    Update

---
+++ Shared/clsGlobal.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DVLD_Shared
+{
+
+    public static class clsGlobal
+    {
+
+        public static string source = "DVLD";
+
+        private static int _eventLogID = (clsLogger.LoadValueFromCurrentUserRegistry("DVLD", "EventID") as int?) ?? 0;
+        public static int EventLogID
+        {
+            get => _eventLogID;
+
+            set => _eventLogID = value;
+        }
+
+ 
+    }
+}
---
+++ Shared/clsLogger.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+
+namespace DVLD_Shared
+{
+    public class clsLogger
+    {
+        public static bool LogIntoEventViewer(string source, string message, EventLogEntryType type, string logName = "Application")
+        {
+            var method = new StackTrace().GetFrame(1).GetMethod();
+            string className = method.DeclaringType.Name ?? "Unknown Class";
+            string methodName = method.Name ?? "Unknown Method";
+            string level = type.ToString();
+
+            string fullMessage = $"[{level}] [{className}] [{methodName}] -> {message}";
+
+            try
+            {
+                if (!EventLog.SourceExists(source))
+                    EventLog.CreateEventSource(source, logName);
+
+                EventLog.WriteEntry(source, fullMessage, type, ++clsGlobal.EventLogID);
+
+                SaveValueIntoCurrentUserRegistry(clsGlobal.source, "EventID", clsGlobal.EventLogID, kind : RegistryValueKind.DWord);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool SaveValueIntoCurrentUserRegistry(string subKeyName, string valueName, object valueData, RegistryValueKind kind = RegistryValueKind.String, string superkeyName = "Software")
+        {
+            string keyPath = $@"{superkeyName}\{subKeyName}";
+
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(keyPath))
+                {
+                    key.SetValue(valueName, valueData, kind);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static object LoadValueFromCurrentUserRegistry(string subKeyName, string valueName, string superkeyName = "Software")
+        {
+            object valueData = null;
+            string keyPath = $@"{superkeyName}\{subKeyName}";
+
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(keyPath))
+                {
+                    valueData = key.GetValue(valueName, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            return valueData;
+        }
+    }
+}
